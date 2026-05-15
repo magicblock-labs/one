44ec6b23b06473e8a63d3f20aee4c5b40080d194
freelook <7212860+freelook@users.noreply.github.com>
2026-05-15 11:49:40 +0200

    feat: use env for sentry config

--- lib/sentry.ts
+++ lib/sentry.ts
@@ -1,3 +1,2 @@
 export const SENTRY_DSN =
-  process.env.NEXT_PUBLIC_SENTRY_DSN?.trim() ||
-  "https://81551955b403808ca0d241b04b855620@o4511348334854144.ingest.us.sentry.io/4511370863181824";
+  process.env.NEXT_PUBLIC_SENTRY_DSN?.trim() || "";
